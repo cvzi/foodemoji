fa2a4939938f149cb0ca4f0c9441c9fa53ef62d1
cvzi <cuzi@openmail.cc>
2018-10-03 23:00:54 +0200

    setup

--- foodemoji/__init__.py
+++ foodemoji/__init__.py
@@ -1,12 +1,53 @@
 ﻿# -*- coding: utf-8 -*-
 # https://github.com/cvzi/foodemoji
+"""
+Decorate a German text (e.g. restaurant menu) with food specific emojis
+https://github.com/cvzi/foodemoji
+
+Unicode country code emoji flags for Python
+~~~~~~~~~~~~~~~~
+    >>> import foodemoji
+    >>> foodemoji.decorate("Apfelrotkraut")
+    'Apfelrotkraut :red_apple:'
+    >>> import emoji
+    >>> emoji.emojize(foodemoji.decorate("Apfelrotkraut"))
+    'Apfelrotkraut 🍎'
+"""
+__version__ = '1.0.0'
+__author__ = 'cuzi'
+__email__ = 'cuzi@openmail.cc' 
+__source__ = 'https://github.com/cvzi/foodemoji'
+__license__ = """
+MIT License
+
+Copyright (c) cuzi 2018
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+__all__ = ['decorate']
+
 import pkg_resources
 import json
 import re
 import sys
 import os
-
-__all__ = ['decorate']
 
 _emoji_re = {}
 _wordend = None
---
+++ setup.py
@@ -0,0 +1,40 @@
+﻿import setuptools
+import os
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read().strip()
+
+version = None
+with open(os.path.join("foodemoji", "__init__.py"), encoding="utf-8") as f:
+    for line in f:
+        if line.strip().startswith("__version__"):
+            version = line.split("=")[1].strip().replace('"', "").replace("'", "")
+            break
+
+setuptools.setup(
+    name="german-foodemoji",
+    version=version,
+    author="cuzi",
+    author_email="cuzi@openmail.cc",
+    description="Decorate German text with food specific emojis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cvzi/foodemoji",
+    packages=["foodemoji"],
+    zip_safe=True,
+    classifiers=(
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Communications :: Chat",
+        "Topic :: Printing",
+        "Topic :: Text Processing :: General"
+    ),
+)
