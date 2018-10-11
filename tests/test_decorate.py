d28de27fd65a5b9459f24fb8e29521f2d02cb812
cvzi <cuzi@openmail.cc>
2018-10-11 13:08:32 +0200

    kürbis

--- tests/test_decorate.py
+++ tests/test_decorate.py
@@ -71,7 +71,7 @@ def test_basic():
         ("Tintenfisch", "Tintenfisch :squid:"), # no fish
         ("Haifisch", "Haifisch :fish:"),
         ("Grießflammerie", "Grießflammerie"), # lamm
-        (u"Kürbis", u"Kürbis :jack-o-lantern:"), # lamm
+        (u"Kürbis", u"Kürbis :jack-o-lantern:")
         
     ]
     for text, text_with_emoji in pairs:
