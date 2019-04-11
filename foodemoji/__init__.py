784195b8291a76b14dccb63fc811d799eb877656
cvzi <cuzi@openmail.cc>
2019-04-11 21:03:15 +0200

    Added emojies from Emoji 11.0

--- foodemoji/__init__.py
+++ foodemoji/__init__.py
@@ -14,7 +14,7 @@ https://github.com/cvzi/foodemoji
     >>> emoji.emojize(foodemoji.decorate("Apfelrotkraut"))
     'Apfelrotkraut 🍎'
 """
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __author__ = 'cuzi'
 __email__ = 'cuzi@openmail.cc'
 __source__ = 'https://github.com/cvzi/foodemoji'
