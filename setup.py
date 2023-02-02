df41a18aa8312a1bfa35290338561415f37842d2
cvzi <cuzi@openmail.cc>
2023-02-02 18:43:53 +0100

    Replace setup.py / setup.cfg with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,51 +1,9 @@
-﻿import setuptools
-import os
-import io
+﻿#!/usr/bin/env python
 
-with io.open("README.md", encoding="utf-8") as f:
-    long_description = f.read().strip()
+from setuptools import setup
 
-version = None
-with io.open(os.path.join("foodemoji", "__init__.py"), encoding="utf-8") as f:
-    for line in f:
-        if line.strip().startswith("__version__"):
-            version = line.split("=")[1].strip()
-            version = version.replace('"', "").replace("'", "")
-            break
-
-setuptools.setup(
-    name="german-foodemoji",
-    version=version,
-    author="cuzi",
-    author_email="cuzi@openmail.cc",
-    license="MIT",
-    description="Decorate German text with food specific emojis",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cvzi/foodemoji",
-    packages=["foodemoji"],
-    package_dir={"foodemoji": "foodemoji"},
-    package_data={"foodemoji": ["foodemojis.json"]},
-    zip_safe=True,
-    test_suite="nose.collector",
-    tests_require=["emoji", "nose", "packaging"],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Communications :: Chat",
-        "Topic :: Printing",
-        "Topic :: Text Processing :: General"
-    ]
-)
+if __name__ == "__main__":
+    setup(
+        name="german-foodemoji",
+        requires=[],
+    )
